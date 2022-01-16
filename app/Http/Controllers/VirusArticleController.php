1d4afa1963ece53b10d2c3a7e3e7df42d39e18b5
NoNutNghia <nghia.nn260701@gmail.com>
2022-01-16 16:21:03 +0700

    Login/Register

---
+++ app/Http/Controllers/VirusArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VirusArticleController extends Controller
+{
+    //
+    public function view() {
+        return "CRUD Admin";
+    }
+}
